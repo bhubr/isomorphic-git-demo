2a890d36639454d9cdbf366c394007092a489f88
Benoît Hubert <benoithubert@gmail.com>
2021-08-27 15:26:08 +0200

    Add calendar view w/ react-big-calendar

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,13 +1,18 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Modal from 'react-modal';
+import { Calendar, Views, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
 import FormEvent from './FormEvent';
 import FormEventClone from './FormEventClone';
 import Select from './components/Select';
 import { deleteEvent } from './store/actions/events';
 import { formatDateMonth } from './helpers/utils';
+import * as dates from './helpers/dates';
+
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const customModalStyles = {
   overlay: {
@@ -19,7 +24,22 @@ const customModalStyles = {
   },
 };
 
+const allViews = Object.keys(Views).map(k => Views[k]);
+
+const localizer = momentLocalizer(moment);
+
+const ColoredDateCellWrapper = ({ children }) =>
+  React.cloneElement(React.Children.only(children), {
+    style: {
+      backgroundColor: 'lightblue',
+    },
+  });
+
 const stripEventId = ({ id, ...event }) => event;
+let i = 1;
+const mapEventToBigCalendar = ({ id, name: title, summary: desc, fullDay: allDay, date, startTime, endTime }) => ({
+  id: i++, desc, title, allDay, start: new Date(`${date} ${startTime}`), end: new Date(`${date} ${endTime}`)
+})
 
 export default function Dashboard() {
   const [editingEvent, setEditingEvent] = useState(null);
@@ -28,7 +48,6 @@ export default function Dashboard() {
   const [eventSummaryFilter, setEventSummaryFilter] = useState('');
   const [eventMonthOptions, setEventMonthOptions] = useState([]);
   const [eventMonthFilter, setEventMonthFilter] = useState('');
-
   const {
     repo: { dir },
     events,
@@ -36,6 +55,7 @@ export default function Dashboard() {
     repo: state.repo,
     events: state.events,
   }));
+  const calendarEvents = useMemo(() => events.map(mapEventToBigCalendar), [events]);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -61,7 +81,6 @@ export default function Dashboard() {
 
   return (
     <main>
-      <h3>Events</h3>
       <Select
         // id, name, label, options, value, onChange
         id="filter-by-summary"
@@ -80,6 +99,22 @@ export default function Dashboard() {
         value={eventMonthFilter}
         onChange={(e) => setEventMonthFilter(e.target.value)}
       />
+
+      <Calendar
+        events={calendarEvents}
+        views={allViews}
+        step={60}
+        showMultiDayTimes
+        max={dates.add(dates.endOf(new Date(2021, 19, 1), 'day'), -1, 'hours')}
+        defaultDate={new Date(2021, 7, 1)}
+        components={{
+          timeSlotWrapper: ColoredDateCellWrapper,
+        }}
+        localizer={localizer}
+      />
+
+      <h3>Events</h3>
+
       <ul>
         {(
           events
