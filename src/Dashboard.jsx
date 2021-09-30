bbb31f1cca1e0ec8bc67146dcafce873a7905ac7
Benoît Hubert <benoithubert@gmail.com>
2021-10-01 00:29:09 +0200

    Filter by customer, memoize, show total per event type

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -3,8 +3,8 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Modal from 'react-modal';
-import { Calendar, Views, momentLocalizer } from 'react-big-calendar'
-import moment from 'moment'
+import { Calendar, Views, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
 import FormEvent from './FormEvent';
 import FormEventClone from './FormEventClone';
 import Select from './components/Select';
@@ -24,7 +24,7 @@ const customModalStyles = {
   },
 };
 
-const allViews = Object.keys(Views).map(k => Views[k]);
+const allViews = Object.keys(Views).map((k) => Views[k]);
 
 const localizer = momentLocalizer(moment);
 
@@ -37,51 +37,111 @@ const ColoredDateCellWrapper = ({ children }) =>
 
 const stripEventId = ({ id, ...event }) => event;
 
-const mapEventToBigCalendar = ({ id, name: title, summary: desc, fullDay: allDay, date, startTime, endTime }) => ({
-  id, desc, title, allDay, start: new Date(`${date} ${startTime}`), end: new Date(`${date} ${endTime}`)
-})
+const mapEventToBigCalendar = ({
+  id,
+  name: title,
+  summary: desc,
+  fullDay: allDay,
+  date,
+  startTime,
+  endTime,
+}) => ({
+  id,
+  desc,
+  title,
+  allDay,
+  start: new Date(`${date} ${startTime}`),
+  end: new Date(`${date} ${endTime}`),
+});
 
 export default function Dashboard() {
   const [editingEvent, setEditingEvent] = useState(null);
   const [cloningEvent, setCloningEvent] = useState(null);
-  const [eventSummaryOptions, setEventSummaryOptions] = useState([]);
   const [eventSummaryFilter, setEventSummaryFilter] = useState('');
-  const [eventMonthOptions, setEventMonthOptions] = useState([]);
+  const [customerFilter, setCustomerFilter] = useState('');
   const [eventMonthFilter, setEventMonthFilter] = useState('');
   const {
     repo: { dir },
     events,
+    customers,
   } = useSelector((state) => ({
     repo: state.repo,
     events: state.events,
+    customers: state.customers,
   }));
-  const calendarEvents = useMemo(() => events.map(mapEventToBigCalendar), [events]);
+  const calendarEvents = useMemo(
+    () => events.map(mapEventToBigCalendar),
+    [events],
+  );
+  const eventSummaryOptions = useMemo(
+    () =>
+      events.reduce(
+        (carry, { summary }) =>
+          carry.find((it) => it.id === summary)
+            ? carry
+            : [...carry, { id: summary, name: summary }],
+        [],
+      ),
+    [events],
+  );
+  const eventMonthOptions = useMemo(
+    () =>
+      events.reduce(
+        (carry, { date }) =>
+          carry.find((it) => it.id === date.substr(0, 7))
+            ? carry
+            : [
+                ...carry,
+                { id: date.substr(0, 7), name: formatDateMonth(date) },
+              ],
+        [],
+      ),
+    [events],
+  );
+  const filteredEvents = useMemo(
+    () =>
+      events
+        .filter((e) => !eventSummaryFilter || e.summary === eventSummaryFilter)
+        .filter(
+          (e) => !eventMonthFilter || e.date.substr(0, 7) === eventMonthFilter,
+        )
+        .filter((e) => !customerFilter || e.customerId === customerFilter),
+    [events, eventSummaryFilter, eventMonthFilter, customerFilter],
+  );
+  const computeDuration = () => 3.5;
+  const reducedEvents = useMemo(
+    () =>
+      filteredEvents
+        .reduce((carry, evt) => {
+          const evtInCarry = carry.find((e) => e.summary === evt.summary);
+          if (!evtInCarry) {
+            return [
+              ...carry,
+              {
+                summary: evt.summary,
+                code: evt.code,
+                total: computeDuration(evt),
+              },
+            ];
+          }
+          const nextTotal = evtInCarry.total + computeDuration(evt);
+          return carry.map((e) =>
+            e.summary === evt.summary ? { ...e, total: nextTotal } : e,
+          );
+        }, [])
+        .sort((e1, e2) => {
+          if (e1.summary < e2.summary) return -1;
+          if (e1.summary > e2.summary) return 1;
+          return 0;
+        }),
+    [filteredEvents],
+  );
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    const eventSumOptions = events.reduce(
-      (carry, { summary }) =>
-        carry.find((it) => it.id === summary)
-          ? carry
-          : [...carry, { id: summary, name: summary }],
-      [],
-    );
-    setEventSummaryOptions(eventSumOptions);
-    const eventMthOptions = events.reduce(
-      (carry, { date }) =>
-        carry.find((it) => it.id === date.substr(0, 7))
-          ? carry
-          : [...carry, { id: date.substr(0, 7), name: formatDateMonth(date) }],
-      [],
-    );
-    setEventMonthOptions(eventMthOptions);
-  }, [events]);
 
   const onDeleteEvent = (eventId) => dispatch(deleteEvent(dir, eventId));
 
   return (
     <main>
-
       <Calendar
         events={calendarEvents}
         views={allViews}
@@ -96,6 +156,15 @@ export default function Dashboard() {
       />
 
       <h3>Events</h3>
+      <Select
+        // id, name, label, options, value, onChange
+        id="filter-by-customer"
+        name="customer"
+        label="Filter by customer"
+        options={customers}
+        value={customerFilter}
+        onChange={(e) => setCustomerFilter(e.target.value)}
+      />
       <Select
         // id, name, label, options, value, onChange
         id="filter-by-summary"
@@ -115,19 +184,21 @@ export default function Dashboard() {
         onChange={(e) => setEventMonthFilter(e.target.value)}
       />
 
+      <table>
+        {reducedEvents.map(({ code, summary, total }) => (
+          <tr key={summary}>
+            <td>{code}</td>
+            <td>{summary}</td>
+            <td>{total}</td>
+          </tr>
+        ))}
+      </table>
+
       <ul>
-        {(
-          events
-            .filter(
-              (e) => !eventSummaryFilter || e.summary === eventSummaryFilter,
-            )
-            .filter(
-              (e) =>
-                !eventMonthFilter || e.date.substr(0, 7) === eventMonthFilter,
-            ) || []
-        ).map((item) => (
+        {(filteredEvents || []).map((item) => (
           <li key={item.id}>
-            {item.date} {item.startTime}-{item.endTime} {item.summary}{' '}
+            {item.date} {item.code} {item.startTime}-{item.endTime}{' '}
+            {item.summary}{' '}
             <button type="button" onClick={() => setEditingEvent(item)}>
               edit
             </button>{' '}
